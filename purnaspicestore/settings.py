046bd1361063e6f6348fd39b10f210058fec08e6
Sourav-Roy-Backend <souravray9001@gmail.com>
2023-09-07 02:03:18 +0600

    3rd time setting change

--- purnaspicestore/settings.py
+++ purnaspicestore/settings.py
@@ -122,8 +122,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT =BASE_DIR / 'static'
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR/ 'media'
+# MEDIA_URL = 'media/'
+# MEDIA_ROOT = BASE_DIR/ 'media'
 
 # STATICFILES_DIRS = [
 #     BASE_DIR/'static'
